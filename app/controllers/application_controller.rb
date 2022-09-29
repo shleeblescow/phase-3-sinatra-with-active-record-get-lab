ab439f180067d833401ba8feeb4452fcc363db10
Shelby Lee <slee337@uw.edu>
2022-09-29 13:56:55 -0600

    commit tree 28350ddb55c36535045e48ecf8a0c521bdc4a357

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    bakedgoods_price = BakedGood.order(price: :desc)
+    bakedgoods_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    bakedgoods_most_price = BakedGood.order(price: :desc).first
+    bakedgoods_most_price.to_json
+  end
 
 end
